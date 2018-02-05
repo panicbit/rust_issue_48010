1ffcec34025519d3fa6242584eeeac1922894f89
panicbit <panicbit.dev@gmail.com>
2018-02-05 03:35:46 +0100

    ICE

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+struct Foo;
+
+pub struct Path<T: Bar> {
+    inner: T::Slice,
+}
+
+pub trait Bar: Sized {
+    type Slice: ?Sized;
+
+    fn open(_: &Path<Self>);
+}
+
+impl Bar for Foo {
+    type Slice = [u8];
+
+    fn open(_: &Path<Self>) {
+        unimplemented!()
+    }
+}
